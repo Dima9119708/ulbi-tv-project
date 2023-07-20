6d03a34913531d1ae0c8b71c92a2f2f09fce56ba
dima <dmitrykh@wizardsdev.com>
2023-07-20 12:39:38 +0300

    Lesson 26.

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -10,7 +10,7 @@ const button = cva(null, {
     variants: {
         intent: {
             [EnumVariantButton.BASE]: [
-                'text-color-primary cursor-pointer',
+                'text-color-primary cursor-pointer text-[20rem]',
             ],
             [EnumVariantButton.OUTLINE]: [
                 'p-[0.4rem_1.5rem]',
