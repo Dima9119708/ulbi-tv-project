216f1175d3624c821ae9437d23a04ceb7d6b47c0
dima <dmitrykh@wizardsdev.com>
2023-07-28 09:55:17 +0300

    Lesson 39

--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { cn } from 'shared/lib/classNames/classNames';
 import { Portal } from 'shared/ui/Portal';
 import { useClose } from 'shared/ui/Modal/lib/hooks/useClose';
